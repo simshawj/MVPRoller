1a2dcf6748b669f4f9650cc018ad71448eb5b9cc
James Simshaw <james.simshaw@gmail.com>
2016-02-24 12:00:12 -0800

    Moved MainActivity to activities package

--- app/src/main/java/com/jamessimshaw/mvproller/activities/MainActivity.java
+++ app/src/main/java/com/jamessimshaw/mvproller/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jamessimshaw.mvproller;
+package com.jamessimshaw.mvproller.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jamessimshaw.mvproller.R;
 
 public class MainActivity extends AppCompatActivity {
 
