2bf6850df31c817ce3e2fa6fce6bd391af0fd6ee
James Simshaw <james.simshaw@gmail.com>
2016-02-26 21:25:21 -0800

    Created a more memory efficient DiceSet for large number of dice. Added Hamcrest library so that I can use the greaterThanOrEqualTo method.

---
+++ app/src/main/java/com/jamessimshaw/mvproller/models/DiceSet.java
@@ -0,0 +1,41 @@
+package com.jamessimshaw.mvproller.models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by james on 2/26/16.
+ */
+public class DiceSet {
+
+    private Map<Integer, Integer> mDice;
+
+    public DiceSet() {
+        mDice = new HashMap<>();
+    }
+
+    public int getDieCount(int sides) {
+        if (mDice.containsKey(sides))
+            return mDice.get(sides);
+        else
+            return 0;
+    }
+
+    public void add(int sides) {
+        mDice.put(sides, getDieCount(sides) + 1);
+    }
+
+    public void remove(int sides) {
+        if (getDieCount(sides) > 0)
+            mDice.put(sides, getDieCount(sides) - 1);
+    }
+
+    public int rollAllDice() {
+        int sum = 0;
+        for(Map.Entry<Integer, Integer> entry : mDice.entrySet()) {
+            for(int i = 0; i < entry.getValue(); i++)
+                sum += Die.roll(entry.getKey());
+        }
+        return sum;
+    }
+}
---
+++ app/src/test/java/com/jamessimshaw/mvproller/models/DiceSetTest.java
@@ -0,0 +1,53 @@
+package com.jamessimshaw.mvproller.models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
+import static org.junit.Assert.*;
+
+/**
+ * Created by james on 2/26/16.
+ */
+public class DiceSetTest {
+
+    private DiceSet mDiceSet;
+
+    @Before
+    public void setup() {
+        mDiceSet = new DiceSet();
+    }
+
+    @Test
+    public void testGetDieCount() {
+        assertEquals(0, mDiceSet.getDieCount(20));
+    }
+
+    @Test
+    public void testAddDie() {
+        mDiceSet.add(20);
+        assertEquals(1, mDiceSet.getDieCount(20));
+        assertEquals(0, mDiceSet.getDieCount(100));
+    }
+
+    @Test
+    public void testRemoveDie() {
+        mDiceSet.add(20);
+        mDiceSet.remove(20);
+        assertEquals(0, mDiceSet.getDieCount(20));
+        mDiceSet.remove(20);
+        assertEquals(0, mDiceSet.getDieCount(20));
+    }
+
+    @Test
+    public void testRollAllDice() {
+        mDiceSet.add(1);
+        mDiceSet.add(1);
+        mDiceSet.add(1);
+        mDiceSet.add(1);
+        mDiceSet.add(1);
+        mDiceSet.add(1);
+        mDiceSet.add(20);
+        assertThat(mDiceSet.rollAllDice(), greaterThanOrEqualTo(7));
+    }
+}
